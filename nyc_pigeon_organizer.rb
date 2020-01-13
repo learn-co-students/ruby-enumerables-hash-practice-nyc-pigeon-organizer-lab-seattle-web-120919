8fb8dbe851fe82ae80c132d2a4a11a19a9faa7c3
kkirby16 <kkirby16@my.whitworth.edu>
2020-01-13 11:15:31 -0800

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
 new_hash = {}
-  
+  #the below 3 lines are where we open the book to lay out all of the data first
 data.each do |key, value| #puts keys of color, gender and lives and the value is their big empty hashes 
   value.each do |new_value, names| #new_value are the colors within the color key, so purple, grey, white, brown. and for each of those it will do the names within those which are the names of the birds
     names.each do |name| #then, for each of the bird names it goes and puts each name. 
@@ -13,13 +13,10 @@ data.each do |key, value| #puts keys of color, gender and lives and the value is
       end
       
       #binding.pry
-
       if !new_hash[name][key] #if the name's key isn't set in new_hash yet 
         new_hash[name][key] = [] #then make the name key of new_hash equal to an empty array. ex: name-alex. key- color. color = [] #with colors in it.
       end
       
-      
-
       new_hash[name][key] << new_value.to_s #this pushes in the #new_values which are the colors into new_hash's name #key's key. 
     end
   end
